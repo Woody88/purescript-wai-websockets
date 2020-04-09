60990acd2d5e9496d237ac2e6f5507ac272c4673
Woodson Delhia <woodsondelhia88@gmail.com>
2020-04-09 12:16:01 +0900

    removed comments and fixed imports

--- examples/Server.purs
+++ examples/Server.purs
@@ -2,22 +2,14 @@ module Examples.Server where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
-import Network.WebSockets.Connection as WS
-import Network.WebSockets.Server (onupgrade)
-import Network.WebSockets.Server as WS
-import Node.HTTP as HTTP
+import Network.WebSockets.Connection (acceptRequest, defaultAcceptRequest, withPingThread) as WS
+import Network.WebSockets.Server (ServerApp, runServer) as WS
+
 
 main :: Effect Unit 
--- main = do 
---     httpsvr <- createHttpServer
---     onerror httpsvr 
---     onupgrade_ httpsvr 
---     HTTP.listen httpsvr {backlog: Nothing, hostname: "localhost",port: 8080} $ Console.log "listening"
 main = do 
      WS.runServer 8080 $ application []
 
